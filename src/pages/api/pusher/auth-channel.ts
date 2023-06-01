5ef99fa3b1f90a06ae2dd2c16b3116dea66cd891
Shayenek <shayenek@gmail.com>
2023-06-01 13:56:30 +0200

    Auth for pusher

---
+++ src/pages/api/pusher/auth-channel.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+
+import { pusherServerClient } from '~/server/pusher'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { channel_name, socket_id } = z
+        .object({ channel_name: z.string(), socket_id: z.string() })
+        .parse(req.body)
+    const { user_id } = req.headers
+
+    if (!user_id || typeof user_id !== 'string') {
+        res.status(404).send('Shit.')
+        return
+    }
+    const auth = pusherServerClient.authorizeChannel(socket_id, channel_name, {
+        user_id,
+        user_info: {
+        name: 'OrMaybeYouHave',
+        },
+    })
+    res.send(auth)
+}
---
+++ src/pages/api/pusher/auth-user.ts
@@ -0,0 +1,19 @@
+import { type NextApiRequest, type NextApiResponse } from 'next'
+import { z } from 'zod'
+
+import { pusherServerClient } from '~/server/pusher'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { socket_id } = z.object({ socket_id: z.string() }).parse(req.body)
+    const { user_id } = req.headers
+
+    if (!user_id || typeof user_id !== 'string') {
+        res.status(404).send('RIP')
+        return
+    }
+    const auth = pusherServerClient.authenticateUser(socket_id, {
+        id: user_id,
+        name: 'YouHaventSeenMeHere',
+    })
+    res.send(auth)
+}
